433024b7c214f0c6485bb5355966b78d9776b724
Przemyslaw Dabrowski <bluh@outlook.com>
2018-10-02 16:31:35 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -16,6 +16,13 @@ class LandmarksController < ApplicationController
 
   get '/landmarks/:id' do
     @landmark = Landmark.find_by(id: params[:id])
-    erb 'landmarks/show'
+    erb :'landmarks/show'
   end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by(id: params[:id])
+    erb :'landmarks/edit'
+  end
+
+  patch '/landmarks/:id'
 end
