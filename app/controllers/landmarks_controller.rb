efbf333f4dec45e1fc5a37311b4d593e3f8640b5
Przemyslaw Dabrowski <bluh@outlook.com>
2018-10-02 16:09:01 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -11,5 +11,10 @@ class LandmarksController < ApplicationController
 
   post '/landmarks' do
     @landmark = Landmark.create(name: params[:landmark]["name"], year_completed: params[:landmark]["year_completed"])
+    redirect "landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by(id: params[:id])
   end
 end
